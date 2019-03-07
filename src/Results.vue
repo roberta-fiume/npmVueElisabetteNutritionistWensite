e495efd37a8628a443f4fdd168d4ac6a668d23c8
Roberta <robertaf898@gmail.com>
2019-03-07 13:43:33 +0000

    Adds counting true answers function with reduce and assign value 0 to each true answer

--- src/Results.vue
+++ src/Results.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="question-and-answers-container">
         <h1>Il tuo risultato è: </h1>
-        <ul id="example-1">
+        <h2>{{result}}</h2>
+        <!-- <ul id="example-1">
             <li v-for="question in this.questions">
                 Question: {{ question.question }}
                 <br>
                 Answer: {{ question.answer }}
             </li>
-        </ul>
+        </ul> -->
     </div>
 </template>
 
@@ -18,12 +19,14 @@
     export default {
         data() {
             return {
-                questions: []
+                questions: [],
+                result: ""
             }
         },
         created() {
-            this.getQuestions();
+            this.countTrueAnswers();
         },
+
         methods: {
             getQuestions() {
                 console.log("My answers are: ", QuestionModel.data.questions);
@@ -34,7 +37,44 @@
                 const answers = this.questions.map(question => question.answer);
 
                 console.log("my answersssss!!!!!!!!!!!!!", answers);
-            }
+
+                return answers
+
+            },
+
+            countTrueAnswers() {
+                console.log("I COUNT THE ANSWERS!!!!");
+                let answers = this.getQuestions();
+                console.log("I'M THE ARRAY OF ANSWERS:", answers);
+                //give 0 points for each TRUE
+                const trueAnswer = "true";
+                const trueAnswers = answers.reduce(function(n, val){
+                    return n + (val === trueAnswer);
+                }, 0)
+                console.log("HERE ARE TRUE ANSWERS", trueAnswers);
+
+
+                let resultYes = trueAnswers * 0
+
+                console.log(" THIS IS RESULT YES", resultYes);
+
+                this.result = resultYes
+
+                return resultYes
+              
+            },
+
+
+             // FOR TOMORROW: ADJUST FUNCTION COUNTTRUEANSWERS
+
+            // countFalseAnswers() {
+            //     //  give 1 point for each FALSE
+            //     const falseAnswer = "false";
+            //     const falseAnswers = answers.reduce(function(n, val) {
+            //         return n + (val === falseAnswer);
+            //     }, 0)
+            //     console.log("HERE ARE FALSE ANSWERS", falseAnswers);
+            // }
         }
     }
 </script>
